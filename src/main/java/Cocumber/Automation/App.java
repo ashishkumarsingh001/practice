24af93d690aeeba0e61313f11bad17e420c6ce88
ASHISH KUMAR SINGH <sashish922@gmail.com>
2021-07-25 17:37:14 +0530

    branching update

--- src/main/java/Cocumber/Automation/App.java
+++ src/main/java/Cocumber/Automation/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("updated");
         System.out.println("update from automation project");
+        System.out.println("new branch update from automation project");
     }
 }
